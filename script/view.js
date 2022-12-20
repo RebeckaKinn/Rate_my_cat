3e6adb5ad8c790411190690142198cf878b6b85c
RebeckaKinn <rebeckaki@getacademy.no>
2022-12-20 14:10:45 +0100

    done

--- script/view.js
+++ script/view.js
@@ -2,7 +2,7 @@ frontPage();
 function frontPage(){
     const frontImage = '<img src="img/mustache_cat.jpg" style="height: 350px; width: auto; border-radius: 80px;" />';
     model.site.innerHTML = /*HTML*/`
-    <div class="headline"><h1>Rate My Pussy!</h1></div>
+    <div class="headline">Rate My Pussy!</div>
     <div class="frontImage">${frontImage}</div>
     <div class="menu">Menu</div>
     <div class="topButtonsDiv">
@@ -30,10 +30,10 @@ function rating(){
         <button class="topButtons" onclick="frontPage()">Back</button>
         </div>
         <div class="ratingName">${model.cats[model.currentCat].name}</div>
-        <button class="leftArrow" onclick="nextCat('left')">←</button>
-        <div class="imageRating"><img src="${model.cats[model.currentCat].img}" style="height: 300px; width: auto"/></div>
-        <p class="ratingPoints">${model.cats[model.currentCat].points}</p>
-        <button class="rightArrow" onclick="nextCat('right')">→</button>
+        <button class="leftArrow" onclick="nextCat('left')"><img src="img/arrowLeft.png" style="height: 100px"/></button>
+        <div class="imageRating"><img src="${model.cats[model.currentCat].img}" style="height: 300px; width: auto; border-radius: 20px;"/></div>
+        <div class="ratingPoints">${model.cats[model.currentCat].points}</div>
+        <button class="rightArrow" onclick="nextCat('right')"><img src="img/arrowRight.png" style="height: 100px"/></button>
         `;
         
         for(let j = 0; j < model.ratingNames.length; j++){
@@ -58,21 +58,21 @@ function topRated(){
             <br/>
             <button class="topButtons" onclick="frontPage()">Back</button>
     </div>
-    <h2 class="top3Ratedtitle">Top 3 rated:</h2>
+    <div class="headline">Top 3 cats</div>
     <div class="top3Rated">
         <h4 class="TopRatedName1">${model.cats[0].name}</h4>
         <p class="TopRatedOwner1">Eier: ${model.cats[0].owner}</p>
-        <div class="TopRatedCat1"><img src="${model.cats[0].img}" style="height: auto; width: 190px"/></div>
+        <div class="TopRatedCat1"><img src="${model.cats[0].img}" style="height: auto; width: 190px; border-radius: 20px;"/></div>
         <p class="TopRatedScore1">${model.cats[0].points}</p>
     
         <h4 class="TopRatedName2">${model.cats[1].name}</h4>
         <p class="TopRatedOwner2">Eier: ${model.cats[1].owner}</p>
-        <div class="TopRatedCat2"><img src="${model.cats[1].img}" style="height: auto; width: 190px"/></div>
+        <div class="TopRatedCat2"><img src="${model.cats[1].img}" style="height: auto; width: 190px; border-radius: 20px;"/></div>
         <p class="TopRatedScore2">${model.cats[1].points}</p>
 
         <h4 class="TopRatedName3">${model.cats[2].name}</h4>
         <p class="TopRatedOwner3">Eier: ${model.cats[2].owner}</p>
-        <div class="TopRatedCat3"><img src="${model.cats[2].img}" style="height: auto; width: 190px"/></div>
+        <div class="TopRatedCat3"><img src="${model.cats[2].img}" style="height: auto; width: 190px; border-radius: 20px;"/></div>
         <p class="TopRatedScore3">${model.cats[2].points}</p>
     </div>
     <div class="allCatsratings">All cats:</div>
@@ -82,7 +82,7 @@ function topRated(){
         <div class="ratedAllTime">
         <h4 class="allCatsRatedName">${model.cats[i].name}</h4>
         <p class="allCatsRatedOwner">Eier: ${model.cats[i].owner}</p>
-        <div class="allCatsRatedImg"><img src="${model.cats[i].img}" style="height: 250px; width: auto"/></div>
+        <div class="allCatsRatedImg"><img src="${model.cats[i].img}" style="height: 250px; width: auto; border-radius: 20px;"/></div>
         <p class="allCatsRatedPoints">${model.cats[i].points}</p>
         </div>
     `;
@@ -94,16 +94,19 @@ function profile(){
         <div class="topButtonsDiv">
             <button class="topButtons" onclick="rating()">Rating</button>
             <button class="topButtons" onclick="topRated()">Rated</button>
-            <button class="topButtons" onclick="logOut()">Log out</button>
+            <button class="topButtons" onclick="logOut()">Logout</button>
             <br/>
             <br/>
             <button class="topButtons" onclick="frontPage()">Back</button>
         </div>
-        <button class="UploadCatImageButton" onclick="uploadCatImageProfile()">Add a new cat!</button>
-        <h3 class="welcomeProfile">Welcome to your profile ${model.currentUser[0].name}</h3>
-        <div class="userImage"><img src="${model.currentUser[0].img}" style="height: 150px; width: auto"/></div>
+        <div class="headline">Profile</div>
+        <div class="welcomeProfile">${model.currentUser[0].name}</div>
+        <div class="userImage"><img src="${model.currentUser[0].img}" style="height: 150px; width: auto; border-radius: 40px;"/></div>
         <div class="userInfoBox">${model.infoToUser}</div>
-        <button class="updateInfoButton" onclick="updateInfoButton()">update info</button>
+        <button class="updateInfoButton" onclick="updateInfoButton()">Update my info</button>
+        <div class="addABewCat">Add a new cat to your family
+        <button class="UploadCatImageButton" onclick="uploadCatImageProfile()">here.</button>
+        </div>
         `;
         for(let j = 0; j < model.cats.length; j++){
             if(model.currentUser[0].name == model.cats[j].owner){
@@ -111,7 +114,7 @@ function profile(){
                 model.site.innerHTML += /*HTML*/`
                 <div class="ratedAllTime">
                 <h4 class="profileCatName">${model.cats[j].name}</h4>
-                <div class="profileCatImg"><img src="${model.cats[j].img}" style="height: 250px; width: auto"/></div>
+                <div class="profileCatImg"><img src="${model.cats[j].img}" style="height: 250px; width: auto; border-radius: 20px;"/></div>
                 <p class="profileCatScore">Score: ${model.cats[j].points}</p>
                 </div>
                 `; 
@@ -127,7 +130,7 @@ function uploadCatImageProfile(){
             <button class="topButtons" onclick="logIn()">Profile</button>
             <br/>
             <br/>
-            <button class="topButtons" onclick="frontPage()">Back</button>
+            <button class="topButtons" onclick="profile()">Back</button>
         </div>
         <div class="logInSite">
         <h3 class="welcomeProfile">Add a new cat!</h3>
@@ -161,7 +164,7 @@ function logIn(){
         <button class="logInnButton" onclick="logInnUser()">Log in</button>
         </div>
         <div class="notLoggedIn">Not have an account yet?</div>
-        <button class="createAccountButton" onclick="createAccount()">Create account</button>
+        <button class="createAccountButton" onclick="createAccount()">Click here!</button>
         `;
     };
 }
@@ -174,7 +177,7 @@ function createAccount(){
         <button class="topButtons" onclick="logIn()">Profile</button>
         <br/>
         <br/>
-        <button class="topButtons" onclick="frontPage()">Back</button>
+        <button class="topButtons" onclick="logIn()">Back</button>
     </div>
     <div class="logInSite">
     <div class="logInBoxName">Create username:</div>
